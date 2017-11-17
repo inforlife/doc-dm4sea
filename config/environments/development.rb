436b7464326517871dadd08cad31ce301c78f6ff
Sigfrid Dusci <sig@kyooem.com>
2017-11-17 17:54:10 +0800

    set email delivery method

---
+++ config/environments/development.rb
@@ -0,0 +1,8 @@
+Mail.defaults do
+  delivery_method :smtp, {  address:        ENV['SMTP_ADDRESS'],
+                            port:           587,
+                            domain:         ENV['SMTP_DOMAIN'],
+                            user_name:      ENV['SMTP_USERNAME'],
+                            password:       ENV['SMTP_PASSWORD'],
+                            authentication: 'login' }
+end

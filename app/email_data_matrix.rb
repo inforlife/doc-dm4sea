039dcca524910010a863b1b8432a990829241c21
Sigfrid Dusci <sig@kyooem.com>
2017-11-17 18:02:25 +0800

    add email data matrix

---
+++ app/email_data_matrix.rb
@@ -0,0 +1,18 @@
+class EmailDataMatrix
+  def self.after_batch_record_print
+    last_batch = CGMPRecord.new('LOTTI').last
+
+    if NewProductionOrderCheck.call(last_batch) && NotYetStoredCheck.call(last_batch)
+      DatabaseRecord.new(batch: last_batch.batch, fdl: false).save
+    else
+      DatabaseRecord.where(fdl: false).each do |record|
+        # CGMPRecord.new('LOTTI').first(batch: record.code) MAY RETURN NIL IF RECORD DELATED IN CGMP
+        if CGMPRecord.new('LOTTI').first(batch: record.code).prt_fdl
+          EmailFile.new(DataMatrixPdf.new(VariableData.new(last_batch)).save).deliver
+          record.update(fdl: true)
+          break
+        end
+      end
+    end
+  end
+end

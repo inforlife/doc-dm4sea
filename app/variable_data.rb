5110ddfe2e584d4903bc2fa00b54f7bbb6dc5854
Sigfrid Dusci <sig@kyooem.com>
2017-11-17 17:58:55 +0800

    add variable data

---
+++ app/variable_data.rb
@@ -0,0 +1,24 @@
+class VariableData
+  def initialize(cgmp_batch)
+    @attributes = [ { name: 'product_code',       ai: 93, value: cgmp_batch.codeprod },
+                    { name: 'batch_code',         ai: 10, value: cgmp_batch.batch },
+                    { name: 'manufacturing_date', ai: 11, value: cgmp_batch.dataprod.strftime('%d/%m/%Y') },
+                    { name: 'expiration_date',    ai: 17, value: cgmp_batch.datascaden.strftime('%d/%m/%Y') },
+                    { name: 'sellable_units',     ai: 37, value: get_sellable_units(cgmp_batch) } ]
+  end
+
+  def to_s
+    @attributes.map { |data| "%#{data[:ai]}%#{data[:value]}" }.join('')
+  end
+
+  def batch_code
+    @attributes.detect { |attribute| attribute[:name] == 'batch_code' }[:value]
+  end
+
+private
+
+  def get_sellable_units(cgmp_batch)
+    formula = CGMPRecord.new('LEGAMI').where(codeprod: cgmp_batch.codeprod, metodopro: cgmp_batch.metodopro)
+    Integer(formula.select { |item| /\A43-\w+\Z/ =~ item.codice_mp }.max_by(&:legame).legame)
+  end
+end

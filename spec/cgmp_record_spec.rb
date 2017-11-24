29760a13f05c7d0d0beb2f4653c8b2aae9ec91e5
Sigfrid Dusci <sig@kyooem.com>
2017-11-24 15:03:27 +0800

    update spec

--- spec/cgmp_record_spec.rb
+++ spec/cgmp_record_spec.rb
@@ -30,11 +30,11 @@ RSpec.describe CGMPRecord do
 
   context '#where' do
     it 'returns all the records the in file with matching attributes' do
-      record = CGMPRecord.new('DUMMY').first(prt_fdl: true)
+      records = CGMPRecord.new('DUMMY').where(prt_fdl: true)
 
-      expect(record.codeprod).to eq('10-000002')
-      expect(record.batch).to eq('LOT2')
-      expect(record.prt_fdl).to eq( true)
+      expect(records.count).to eq(2)
+      expect(records.first.batch).to eq('LOT2')
+      expect(records.last.batch).to eq('LOT3')
     end
   end
 end

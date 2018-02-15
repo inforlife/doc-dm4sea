566ee6e1bc66f8cda8cddf82fb6e6799c067290f
Sigfrid Dusci <sig@inforlife.ch>
2018-02-15 10:42:29 +0100

    Skip 790

--- spec/bag_check_spec.rb
+++ spec/bag_check_spec.rb
@@ -7,15 +7,21 @@ RSpec.describe BagCheck do
     end
 
     it 'returns false when record has codeprod starting with 56-' do
-      cgmp_batch_0 = spy('cGMP batch', codeprod: '56-xxxxxx')
+      cgmp_batch_1 = spy('cGMP batch', codeprod: '56-xxxxxx')
 
-      expect(BagCheck.call(cgmp_batch_0)).to eq(false)
+      expect(BagCheck.call(cgmp_batch_1)).to eq(false)
+    end
+
+    it 'returns false when record has codeprod starting with 790' do
+      cgmp_batch_2 = spy('cGMP batch', codeprod: '790xxx')
+
+      expect(BagCheck.call(cgmp_batch_2)).to eq(false)
     end
 
     it 'returns true otherwise' do
-      cgmp_batch_1 = spy('cGMP batch', codeprod: '51-xxxxxx')
+      cgmp_batch_3 = spy('cGMP batch', codeprod: '51-xxxxxx')
 
-      expect(BagCheck.call(cgmp_batch_1)).to eq(true)
+      expect(BagCheck.call(cgmp_batch_3)).to eq(true)
     end
   end
 end

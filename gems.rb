9c322cba3ef455b4e458e3d83c31f91ab122c61e
Sigfrid Dusci <sig@inforlife.ch>
2018-05-23 10:09:18 +0200

    process datamatix on batches

--- gems.rb
+++ gems.rb
@@ -1,11 +1,11 @@
 source 'https://rubygems.org'
 
-gem 'listen',   '3.1.5'
-gem 'dbf',      '3.1.1'
-gem 'sqlite3',  '1.3.13'
-gem 'rake',     '11.3.0'
-gem 'prawn',    '2.2.0'
-gem 'mail',     '2.7.0'
+gem 'rufus-scheduler',  '3.5.0'
+gem 'dbf',              '3.1.1'
+gem 'sqlite3',          '1.3.13'
+gem 'rake',             '11.3.0'
+gem 'prawn',            '2.2.0'
+gem 'mail',             '2.7.0'
 
 group :production do
   gem 'rollbar',  '2.15.5'

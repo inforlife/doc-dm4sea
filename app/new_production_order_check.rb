a2aa433efeea1603714258299f917bd1b99c4376
Sigfrid Dusci <sig@inforlife.ch>
2018-05-23 10:10:06 +0200

    creates datamtrixes for batch records printed today

--- app/new_production_order_check.rb
+++
@@ -1,5 +0,0 @@
-class NewProductionOrderCheck
-  def self.call(cgmp_batch)
-    cgmp_batch.stato == '0' && !cgmp_batch.datascaden.nil?
-  end
-end
--- spec/email_data_matrix_spec.rb
+++ spec/email_data_matrix_spec.rb
@@ -1,101 +1,88 @@
 RSpec.describe EmailDataMatrix do
-  let(:cgmp_table_class)      { class_spy('CGMPRecord').as_stubbed_const }
-  let(:cgmp_table)            { spy('cGMP table') }
-  let(:database_record_class) { class_spy('DatabaseRecord').as_stubbed_const }
-  let(:database_record)       { spy('database record', batch: 'LOT2') }
-  let(:log_action_class)      { class_spy('LogAction').as_stubbed_const }
-  let(:log_action)            { spy('email file') }
-  let(:email_file_class)      { class_spy('EmailFile').as_stubbed_const }
-  let(:email_file)            { spy('email file') }
-  let(:data_matrix_pdf_class) { class_spy('DataMatrixPdf').as_stubbed_const }
-  let(:data_matrix_pdf)       { spy('datamatrix pdf') }
-  let(:variable_data_class)   { class_spy('VariableData').as_stubbed_const }
+  let(:cgmp_table_class)            { class_spy('CGMPRecord').as_stubbed_const }
+  let(:cgmp_table)                  { spy('cGMP table') }
+  let(:cgmp_record)                 { spy('cGMP record', batch: 'LOT1') }
+  let(:database_record_class)       { class_spy('DatabaseRecord').as_stubbed_const }
+  let(:database_record)             { spy('database record') }
+  let(:log_action_class)            { class_spy('LogAction').as_stubbed_const }
+  let(:log_action)                  { spy('email file') }
+  let(:email_file_class)            { class_spy('EmailFile').as_stubbed_const }
+  let(:email_file)                  { spy('email file') }
+  let(:data_matrix_pdf_class)       { class_spy('DataMatrixPdf').as_stubbed_const }
+  let(:data_matrix_pdf)             { spy('datamatrix pdf') }
+  let(:variable_data_class)         { class_spy('VariableData').as_stubbed_const }
+  let(:bag_check_class)             { class_spy('BagCheck').as_stubbed_const }
+  let(:not_yet_stored_check_class)  { class_spy('NotYetStoredCheck').as_stubbed_const }
 
   before do
     allow(cgmp_table_class).to receive(:new) { cgmp_table }
   end
 
-  context '::after_batch_record_print' do
-    let(:new_production_order_check_class) { class_spy('NewProductionOrderCheck').as_stubbed_const }
-    let(:bag_check_class)                  { class_spy('BagCheck').as_stubbed_const }
-    let(:not_yet_stored_check_class)       { class_spy('NotYetStoredCheck').as_stubbed_const }
-    let(:last_cgmp_record)                 { spy('Last cGMP record', batch: 'LOT1') }
-
-    before do
-      allow(new_production_order_check_class).to receive(:call) { true }
-      allow(not_yet_stored_check_class).to receive(:call)       { true }
-      allow(bag_check_class).to receive(:call)                  { true }
-      allow(database_record_class).to receive(:new)             { database_record }
-      allow(cgmp_table).to receive(:last)                       { last_cgmp_record }
+  context '::for_batch_records_printed_today' do
+    it 'fetches from LOTTI the records for open production orders with printed batch record' do
+      EmailDataMatrix.for_batch_records_printed_today
+      expect(cgmp_table).to have_received(:where).with({ stato: '0', prt_fdl: true })
     end
 
-    context 'it checks' do
-      it 'a new production order has been created' do
-        EmailDataMatrix.after_batch_record_print
-        expect(new_production_order_check_class).to have_received(:call).with(last_cgmp_record)
-      end
-      it 'the production order is for a lot of bags' do
-        EmailDataMatrix.after_batch_record_print
-        expect(bag_check_class).to have_received(:call).with(last_cgmp_record)
-      end
-
-      it 'the production order has not been stored yet' do
-        EmailDataMatrix.after_batch_record_print
-        expect(not_yet_stored_check_class).to have_received(:call).with(last_cgmp_record)
-      end
-    end
-
-    context 'when the modification to LOTTI is the addition of a new bag production order not processed yet' do
-      it 'adds the batch to database with fdl set to false' do
-        EmailDataMatrix.after_batch_record_print
-        expect(database_record_class).to have_received(:new).with(batch: 'LOT1', fdl: false)
-        expect(database_record).to have_received(:save)
-      end
-    end
-
-    context 'when the modification to LOTTI is not the addition of a new production order' do
+    context 'for each record' do
       before do
-        allow(new_production_order_check_class).to receive(:call)   { false }
+        allow(EmailDataMatrix).to receive(:generate_and_email_datamatrix_for)
+        allow(cgmp_table).to receive(:where)                { [cgmp_record] }
+        allow(database_record_class).to receive(:new)       { database_record }
+        allow(not_yet_stored_check_class).to receive(:call) { true }
+        allow(bag_check_class).to receive(:call)            { true }
       end
 
-      context 'and a batch record has been printed for a batch which previously did not have the batch record printed' do
-        let(:cgmp_record) { spy('cGMP record', prt_fdl: true) }
+      it 'checks the production order is for a lot of bags' do
+        EmailDataMatrix.for_batch_records_printed_today
+        expect(bag_check_class).to have_received(:call).with(cgmp_record)
+      end
 
-        before do
-          allow(database_record_class).to receive(:where)           { [database_record] }
-          allow(cgmp_table).to receive(:first).with(batch: 'LOT2')  { cgmp_record }
-          allow(data_matrix_pdf_class).to receive(:new)             { data_matrix_pdf }
-        end
+      it 'checks the production order has not been processed yet' do
+        EmailDataMatrix.for_batch_records_printed_today
+        expect(not_yet_stored_check_class).to have_received(:call).with(cgmp_record)
+      end
+    end
 
-        it 'generates the datamatrix for the batch' do
-          allow(email_file_class).to receive(:new)    { email_file }
-          allow(variable_data_class).to receive(:new)
+    context 'when the record is a new bag production order not yet processed' do
+      before do
+        allow(cgmp_table).to receive(:where)                { [cgmp_record] }
+        allow(data_matrix_pdf_class).to receive(:new)       { data_matrix_pdf }
+        allow(not_yet_stored_check_class).to receive(:call) { true }
+        allow(bag_check_class).to receive(:call)            { true }
 
-          EmailDataMatrix.after_batch_record_print
-          expect(data_matrix_pdf).to have_received(:save)
-          expect(variable_data_class).to have_received(:new).with(cgmp_record)
-        end
+        allow(variable_data_class).to receive(:new)
+        allow(database_record_class).to receive(:new)       { database_record }
+      end
 
-        it 'emails the datamatrix' do
-          allow(email_file_class).to receive(:new) { email_file }
+      it 'generates the datamatrix for the batch' do
+         allow(email_file_class).to receive(:new)    { email_file }
 
-          EmailDataMatrix.after_batch_record_print
-          expect(email_file).to have_received(:deliver)
-        end
+        EmailDataMatrix.for_batch_records_printed_today
+        expect(data_matrix_pdf).to have_received(:save)
+        expect(variable_data_class).to have_received(:new).with(cgmp_record)
+      end
 
-        it 'logs the email send' do
+      it 'emails the datamatrix' do
+        allow(email_file_class).to receive(:new) { email_file }
+
+        EmailDataMatrix.for_batch_records_printed_today
+        expect(email_file).to have_received(:deliver)
+      end
+
+      it 'logs the email send' do
           allow(log_action_class).to receive(:new) { log_action }
 
-          EmailDataMatrix.after_batch_record_print
+          EmailDataMatrix.for_batch_records_printed_today
           expect(log_action).to have_received(:deliver)
-        end
+      end
 
-        it 'updates the batch setting fdl to true' do
-          allow(email_file_class).to receive(:new) { email_file }
+      it 'adds the batch to database' do
+        allow(email_file_class).to receive(:new) { email_file }
 
-          EmailDataMatrix.after_batch_record_print
-          expect(database_record).to have_received(:update).with(fdl: true)
-        end
+        EmailDataMatrix.for_batch_records_printed_today
+        expect(database_record_class).to have_received(:new).with(batch: cgmp_record.batch, fdl: true)
+        expect(database_record).to have_received(:save)
       end
     end
   end

d1131f26d61ada38db5c06b813e188af3a22e78b
Sigfrid Dusci <sig@inforlife.ch>
2018-05-23 11:37:19 +0200

    fetch number of sellable units from imp_prod

--- spec/variable_data_spec.rb
+++ spec/variable_data_spec.rb
@@ -3,20 +3,22 @@ RSpec.describe VariableData do
                                               batch: 'LOT1',
                                               dataprod: Date.new(2017,11,18),
                                               datascaden: Date.new(2020,11,18),
-                                              qt_teorica: 0.334,
                                               numero_po: '001234567890') }
   let(:cgmp_table_class)  { class_spy('CGMPRecord').as_stubbed_const }
   let(:cgmp_table)        { spy('cGMP table') }
-  let(:item_formula)      { spy('item formula', codice_mp: '43-000000', legame: 100) }
+  let(:imp_prod1)         { spy('imp prod 1', codice_mp: '21-000000', qta_reale: 1000) }
+  let(:imp_prod2)         { spy('imp prod 2', codice_mp: '43-000001', qta_reale: 99) }
+  let(:imp_prod3)         { spy('imp prod 3', codice_mp: '43-000002', qta_reale: 50) }
+  let(:imp_prod4)         { spy('imp prod 4', codice_mp: '43-000002', qta_reale: 50) }
 
   before do
-    allow(cgmp_table_class).to receive(:new).with('LEGAMI') { cgmp_table }
-    allow(cgmp_table).to receive(:where)                    { [item_formula] }
+    allow(cgmp_table_class).to receive(:new).with('IMP_PROD') { cgmp_table }
+    allow(cgmp_table).to receive(:where)                      { [imp_prod1, imp_prod2, imp_prod3, imp_prod4] }
   end
 
   context '#to_s' do
     it 'returns its attributes as string' do
-      expect(VariableData.new(cgmp_batch).to_s).to eq('%93%12-000000%10%LOT1%11%18/11/2017%17%18/11/2020%37%34%400%001234567890')
+      expect(VariableData.new(cgmp_batch).to_s).to eq('%93%12-000000%10%LOT1%11%18/11/2017%17%18/11/2020%37%100%400%001234567890')
     end
   end
 

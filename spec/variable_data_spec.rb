fb90ba49fbc81ad7e5351865a2b51ee9220cc211
Sigfrid Dusci <sig@inforlife.ch>
2018-06-04 09:55:14 +0200

    return always an integer

--- spec/variable_data_spec.rb
+++ spec/variable_data_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe VariableData do
   let(:imp_prod1)         { spy('imp prod 1', codice_mp: '21-000000', qta_reale: 1000) }
   let(:imp_prod2)         { spy('imp prod 2', codice_mp: '43-000001', qta_reale: 99) }
   let(:imp_prod3)         { spy('imp prod 3', codice_mp: '43-000002', qta_reale: 50) }
-  let(:imp_prod4)         { spy('imp prod 4', codice_mp: '43-000002', qta_reale: 50) }
+  let(:imp_prod4)         { spy('imp prod 4', codice_mp: '43-000002', qta_reale: 49.2) }
 
   before do
     allow(cgmp_table_class).to receive(:new).with('IMP_PROD') { cgmp_table }
